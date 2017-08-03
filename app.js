684e096eac064ca8239d3267619b6239eeec7fd0
Philip <phils89144@gmail.com>
2017-08-02 20:41:33 -0700

    second update

--- app.js
+++ app.js
@@ -14,57 +14,40 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(expressValidator())
 app.use(expressSession({secret: "abcdefg"}))
 app.use(express.static(path.join(__dirname, 'static')))
-//app.use(expressSession({secret:'somesecrettokenhere'}));
+
 var username = 'foo'
 var password = 'bar'
 
-//app.get("/", function(req, res, next){
-  //res.redirect('/login');
- //res.render("index", {thankyou:req.query.thankyou})
-//check if loggedin, if true, send to root, if not send to log in page
+//verify logged in:
 function isLoggedIn(req, res, next) {
   if (req.session.isLoggedIn) {
     return next();
   }
+  //if not logged in rediret to login page:
   else {
     res.redirect('/login');
   }
 }
-
-//Do this line after a successfull login:
+//After a successfull login:
 app.get("/", isLoggedIn, function(req, res, next){
   res.render('index', req.session)
 });
 
-// app.get('/login', function(req, res, next) {
-//   res.render('login');
-// });
 
-//check to see if user is loged in
-// function isLoggedIn(req, res, next) {
-//   if (req.session.isAuthenticated){
-    //if logged in then move on
-    //     return next();
-    // } else {
-    // if not logged in then redirect to the /login
-  //    res.redirect('/login');
-//     }
-// }
+//Why doesn't this validation work?
 
-//validate user name and password
-//    req.checkbody(username).isIn('username','foo')
+//validate the user name and password
+//  app.post("/user/add", function(req, res, next){
+//    req.checkbody(name).isIn(username,'foo');
 //      if (true){
-//    req.checkbody(password).isIn(username)
+//    req.checkbody(password).isIn(password, 'bar');
 //    if (true){
-//      then res.redirect('/index')
+//      then res.redirect('/index');
 //    }else{
-// //     errorMessage: "Invalid Username/password";
-//   }
+//     errorMessage: "Invalid Username/password";
+//   }}
 // }
-//return error messages and stay on login page
-//after successfull login show logged in with username
-//The valid usernames and passwords should be kept as a data structure in your application.
-//does this mean use an existing data file?
+
 var users = []
 
 app.post("/user/add", function(req, res, next){
@@ -79,7 +62,6 @@ app.post("/user/add", function(req, res, next){
       console.log(users)
       res.render("/login", {erros: errors})
       //return;
-      //res.send(errors)
       //change the .send to put a message
       //on the login page that there is an error
     } else {
